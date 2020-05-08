4f16690f359c52372d22fa467b2d925bd27050f3
Igor Bitsan <43330739+Igor1488228@users.noreply.github.com>
2020-05-08 13:47:08 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -21,6 +21,6 @@ CREATE TABLE Universityes_Rank (
     ,university_position NUMBER(3,0) NOT NULL
     ,country_name VARCHAR(20) NOT NULL
     ,CONSTRAINT PK_Universityes_Rank PRIMARY KEY (university_name, dynamic_year, country_name, university_position)
-    ,CONSTRAINT FK1_Universityes_Rank FOREIGN KEY (university_position) REFERENCES Ranks(university_position)
+    ,CONSTRAINT FK1_Universityes_Rank FOREIGN KEY (university_position) REFERENCES Rank(university_position)
     ,CONSTRAINT FK2_Universityes_Rank FOREIGN KEY (university_name, dynamic_year, country_name) REFERENCES Universityes(university_name, dynamic_year, country_name)
 );
