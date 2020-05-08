f4ef71365bd334e0d0b16ede456537b1875533ab
Igor Bitsan <43330739+Igor1488228@users.noreply.github.com>
2020-05-08 13:45:40 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,140 +1,87 @@
+INSERT INTO Countries (country_name) VALUES ('United Kingdom');
+INSERT INTO Countries (country_name) VALUES ('Japan');
+INSERT INTO Countries (country_name) VALUES ('USA');
+INSERT INTO Countries (country_name) VALUES ('Switzerland');
+INSERT INTO Countries (country_name) VALUES ('Israel');
 
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('Harvard University', 'United States', '1');
 
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('Harvard University', 'United States', '2012');
+INSERT INTO Rank (university_position) VALUES ('1');
+INSERT INTO Rank (university_position) VALUES ('2');
+INSERT INTO Rank (university_position) VALUES ('3');
+INSERT INTO Rank (university_position) VALUES ('4');
+INSERT INTO Rank (university_position) VALUES ('5');
+INSERT INTO Rank (university_position) VALUES ('6');
+INSERT INTO Rank (university_position) VALUES ('7');
+INSERT INTO Rank (university_position) VALUES ('8');
+INSERT INTO Rank (university_position) VALUES ('9');
+INSERT INTO Rank (university_position) VALUES ('10');
+INSERT INTO Rank (university_position) VALUES ('11');
+INSERT INTO Rank (university_position) VALUES ('12');
+INSERT INTO Rank (university_position) VALUES ('13');
+INSERT INTO Rank (university_position) VALUES ('14');
+INSERT INTO Rank (university_position) VALUES ('15');
+INSERT INTO Rank (university_position) VALUES ('16');
+INSERT INTO Rank (university_position) VALUES ('17');
+INSERT INTO Rank (university_position) VALUES ('18');
+INSERT INTO Rank (university_position) VALUES ('19');
+INSERT INTO Rank (university_position) VALUES ('20');
+INSERT INTO Rank (university_position) VALUES ('21');
+INSERT INTO Rank (university_position) VALUES ('22');
+INSERT INTO Rank (university_position) VALUES ('23');
+INSERT INTO Rank (university_position) VALUES ('24');
+INSERT INTO Rank (university_position) VALUES ('25');
 
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('Harvard University', 'United States');
 
-INSERT INTO years
-(university_name, year)
-VALUES
-('Harvard University', '2012');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Harvard University', 2012, 1, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Massachusetts Institute of Technology', 2012, 3, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Stanford University', 2012, 5, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Cambridge', 2012, 4, 'United Kingdom');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('California Institute of Technology', 2012, 7, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Princeton University', 2012, 2, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Oxford', 2012, 9, 'United Kingdom');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Yale University', 2012, 12, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Columbia University', 2012, 10, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, Berkeley', 2012, 6, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Chicago', 2012, 8, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Cornell University', 2012, 14, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Pennsylvania', 2012, 24, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Tokyo', 2012, 31, 'Japan');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Johns Hopkins University', 2012, 20, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Swiss Federal Institute of Technology in Zurich', 2012, 11, 'Switzerland');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Kyoto University', 2012, 19, 'Japan');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Weizmann Institute of Science', 2012, 22, 'Israel');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, Los Angeles', 2012, 23, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, San Diego', 2012, 15, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Rockefeller University', 2012, 16, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Hebrew University of Jerusalem', 2012, 13, 'Israel');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('New York University', 2012, 17, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, San Francisco', 2012, 21, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Wisconsin–Madison', 2012, 33, 'USA');
 
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('Massachusetts Institute of Technology', 'United States', '2');
 
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('Massachusetts Institute of Technology', 'United States', '2012');
-
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('Massachusetts Institute of Technology', 'United States');
-
-INSERT INTO years
-(university_name, year)
-VALUES
-('Massachusetts Institute of Technology', '2012');
-
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('Stanford University', 'United States', '3');
-
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('Stanford University', 'United States', '2012');
-
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('Stanford University', 'United States');
-
-INSERT INTO years
-(university_name, year)
-VALUES
-('Stanford University', '2012');
-
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('University of Cambridge', '	United Kingdom', '4');
-
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('University of Cambridge', '	United Kingdom', '2012');
-
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('University of Cambridge', '	United Kingdom');
-
-INSERT INTO years
-(university_name, year)
-VALUES
-('University of Cambridge', '2012');
-
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('California Institute of Technology', 'United States', '5');
-
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('California Institute of Technology', 'United States', '2012');
-
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('California Institute of Technology', 'United States');
-
-INSERT INTO years
-(university_name, year)
-VALUES
-('California Institute of Technology', '2012');
-
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('Princeton University', 'United States', '6');
-
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('Princeton University', 'United States', '2012');
-
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('Princeton University', 'United States');
-
-INSERT INTO years
-(university_name, year)
-VALUES
-('Princeton University', '2012');
-
-INSERT INTO university
-(university_name, country, position )
-VALUES
-('University of Oxford', '	United Kingdom', '7');
-
-INSERT INTO position
-(university_name, country, year)
-VALUES
-('University of Oxford', '	United Kingdom', '2012');
-
-INSERT INTO countryes
-(university_name, country)
-VALUES
-('University of Oxford', '	United Kingdom');
-
-INSERT INTO years
-(university_name, year)
-VALUES
-('University of Oxford', '2012');
+INSERT INTO Bands_Genres (band_name, formed_year, country_name, genre_name) VALUES ('Iron Maiden', 1975, 'United Kingdom', 'New wave of british heavy');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Harvard University', 2012, 1, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Massachusetts Institute of Technology', 2012, 3, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Stanford University', 2012, 5, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Cambridge', 2012, 4, 'United Kingdom');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('California Institute of Technology', 2012, 7, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Princeton University', 2012, 2, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Oxford', 2012, 9, 'United Kingdom');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Yale University', 2012, 12, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Columbia University', 2012, 10, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, Berkeley', 2012, 6, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Chicago', 2012, 8, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Cornell University', 2012, 14, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Pennsylvania', 2012, 24, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Tokyo', 2012, 31, 'Japan');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Johns Hopkins University', 2012, 20, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Swiss Federal Institute of Technology in Zurich', 2012, 11, 'Switzerland');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Kyoto University', 2012, 19, 'Japan');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Weizmann Institute of Science', 2012, 22, 'Israel');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, Los Angeles', 2012, 23, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, San Diego', 2012, 15, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Rockefeller University', 2012, 16, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('Hebrew University of Jerusalem', 2012, 13, 'Israel');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('New York University', 2012, 17, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of California, San Francisco', 2012, 21, 'USA');
+INSERT INTO Universityes (university_name, dynamic_year, quality_of_faculty, country_name) VALUES ('University of Wisconsin–Madison', 2012, 33, 'USA');
