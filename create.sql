644022bb649f4a45be75c01844db1040df75b917
Igor Bitsan <43330739+Igor1488228@users.noreply.github.com>
2020-05-09 03:45:22 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -6,21 +6,21 @@ CREATE TABLE Rank (
     university_position Number(3,0) NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE Universityes (
+CREATE TABLE Universities (
      university_name VARCHAR(50) NOT NULL
     ,dynamic_year NUMBER(4,0) NOT NULL
     ,quality_of_faculty NUMBER(4,0) 
     ,country_name VARCHAR(20) NOT NULL REFERENCES Countries(country_name)
     ,CONSTRAINT CHK_Years CHECK (dynamic_year > 0 AND quality_of_faculty > 0)
-    ,CONSTRAINT PK_Universityes PRIMARY KEY (university_name, dynamic_year, country_name)
+    ,CONSTRAINT PK_Universities PRIMARY KEY (university_name, dynamic_year, country_name)
 );
 
-CREATE TABLE Universityes_Rank (
+CREATE TABLE Universities_Rank (
      university_name VARCHAR(50) NOT NULL
     ,dynamic_year NUMBER(4,0) NOT NULL
     ,university_position NUMBER(3,0) NOT NULL
     ,country_name VARCHAR(20) NOT NULL
-    ,CONSTRAINT PK_Universityes_Rank PRIMARY KEY (university_name, dynamic_year, country_name, university_position)
-    ,CONSTRAINT FK1_Universityes_Rank FOREIGN KEY (university_position) REFERENCES Rank(university_position)
-    ,CONSTRAINT FK2_Universityes_Rank FOREIGN KEY (university_name, dynamic_year, country_name) REFERENCES Universityes(university_name, dynamic_year, country_name)
+    ,CONSTRAINT PK_Universities_Rank PRIMARY KEY (university_name, dynamic_year, country_name, university_position)
+    ,CONSTRAINT FK1_Universities_Rank FOREIGN KEY (university_position) REFERENCES Rank(university_position)
+    ,CONSTRAINT FK2_Universities_Rank FOREIGN KEY (university_name, dynamic_year, country_name) REFERENCES Universities(university_name, dynamic_year, country_name)
 );
