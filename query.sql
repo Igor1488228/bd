500b55f9bc8f368fd4d3657c354f3cebae898056
Igor Bitsan <43330739+Igor1488228@users.noreply.github.com>
2020-05-24 19:38:56 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,31 +2,31 @@
 
 SELECT
     TRIM(c.country_name) country,
-    COUNT(u.university_name) universityes
+    COUNT(u.university_name) universities
 FROM
     Countries   c
-    LEFT JOIN Universityes u ON c.country_name = u.country_name
+    LEFT JOIN Universities u ON c.country_name = u.country_name
 GROUP BY
     TRIM(c.country_name)
 ORDER BY
-   universityes DESC;
+   universities DESC;
 
 
 --Second 
 
 SELECT
     TRIM(p.university_position) rank,
-    COUNT(u.university_name) universityes
+    COUNT(u.university_name) universities
 FROM
     rank p
-    LEFT JOIN Universityes_Rank up ON p.university_position = up.university_position
-    LEFT JOIN Universityes u         ON u.university_name = up.university_name
+    LEFT JOIN Universities_Rank up ON p.university_position = up.university_position
+    LEFT JOIN Universities u         ON u.university_name = up.university_name
                              AND u.dynamic_year = up.dynamic_year
                              AND u.country_name = up.country_name
 GROUP BY
     TRIM(p.university_position)
 ORDER BY
-    universityes DESC;
+    universities DESC;
 
 
 --Third 
@@ -36,7 +36,7 @@ SELECT
     NVL(SUM(u.quality_of_faculty), 0) quality_of_faculty
 FROM 
     Countries c
-    LEFT JOIN Universityes u ON c.country_name = u.country_name
+    LEFT JOIN Universities u ON c.country_name = u.country_name
 GROUP BY 
     TRIM(c.country_name)
 ORDER BY 
